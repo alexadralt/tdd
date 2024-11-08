913b724f1864ad198b91713ad788e8518bc9ff6e
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-11-08 14:46:05 +0500

    created project TagsCloudVisualization

---
+++ TagsCloudVisualization/TagsCloudVisualization.Tests/CircularCloudLayouterTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using TagsCloudVisualization;
+
+namespace TagsCloudVisualization.Tests;
+
+[TestFixture]
+[TestOf(typeof(CircularCloudLayouter))]
+public class CircularCloudLayouterTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
