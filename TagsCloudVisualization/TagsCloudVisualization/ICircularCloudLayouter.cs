b4a2cd45174e0a181d2e075ecdfad083d6c6f79d
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-11-16 18:17:20 +0500

    added Layout property

--- TagsCloudVisualization/TagsCloudVisualization/ICircularCloudLayouter.cs
+++ TagsCloudVisualization/TagsCloudVisualization/ICircularCloudLayouter.cs
@@ -6,4 +6,5 @@ public interface ICircularCloudLayouter
 {
     public Rectangle PutNextRectangle(Size rectangleSize);
     public Point CloudCenter { get; set; }
+    public IEnumerable<Rectangle> Layout { get; }
 }
