9fe17b8b5876211fcc02e1e1b5bb6efb8c443fbd
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-11-16 17:04:39 +0500

    added code for image generation

--- TagsCloudVisualization/TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using TagsCloudVisualization;
 
-Console.WriteLine("Hello, World!");
+var sizes = new List<Size>();
+
+var random = new Random();
+for (int i = 0; i < 50; i++)
+{
+    sizes.Add(new Size(random.Next(10, 50), random.Next(10, 50)));
+}
+
+var layout = new CircularCloudLayouterImpl(new Point(1000, 1000))
+    .GenerateLayout(sizes.ToArray())
+    .ToArray();
+
+#pragma warning disable CA1416
+
+var filename = "./test.png";
+new Bitmap(2000, 2000)
+    .DrawRectangles(layout, new Pen(Color.Blue))
+    .Save(filename, ImageFormat.Png);
+Console.WriteLine($"File was saved to: {Path.GetFullPath(filename)}");
+
+#pragma warning restore CA1416
